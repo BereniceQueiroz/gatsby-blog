eae63ecd0dbb6f76967305b10834c9592bdcf79b
BereniceQueiroz <berenice_queiroz@yahoo.com.br>
2020-08-10 18:44:21 -0300

    Criaçao templates post e list-blog

--- gatsby-node.js
+++ gatsby-node.js
@@ -22,31 +22,57 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
   }
 }
 
-//Método para criação de página
+//Método para criação de página e paginaçao
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
 
   //O graphql trará a query para busca dos slugs e a partir do slug criar a página
   return graphql(`
     {
-      allMarkdownRemark {
+      allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
         edges {
           node {
             fields {
               slug
             }
+            frontmatter {
+              background
+              category
+              date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+              description
+              title
+            }
+            timeToRead
           }
         }
       }
     }
   `).then(result => { //assincrono assim que terminar de buscar os nodes fará uma outra açao
-    result.data.allMarkdownRemark.edges.forEach(({ node }) => { // passará por cada node e fará a criaçao da page
+    const posts = result.data.allMarkdownRemark.edges
+
+    posts.forEach(({ node }) => { // passará por cada node e fará a criaçao da page 
       createPage({
         path: node.fields.slug, // caminho, nome do node
         component: path.resolve("./src/templates/blog-post.js"), //template do post fazer o import do path ele pega o caminho correto da pasta
         context: { // permite passar qq dado para o component, por exemplo, timetoRead, slug
           slug: node.fields.slug
         }
+      })
+    })
+
+    const postsPerPage = 6
+    const numPages = Math.ceil(posts.length / postsPerPage)
+
+    Array.from({ length: numPages }).forEach((_, index) => {
+      createPage({
+        path: index === 0 ? `/` : `/page/${index + 1}`,
+        component: path.resolve(`./src/templates/blog-list.js`),
+        context: {
+          limit: postsPerPage,
+          skip: index * postsPerPage,
+          numPages,
+          currentPage: index + 1,
+        },
       })
     })
   })
---
+++ src/components/Pagination/pagination.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import { Link } from 'gatsby';
+import * as S from './pagination.styles';
+
+const Pagination = ({ isFirst, isLast, currentPage, numPages, prevPage, nextPage }) => (
+  <S.PaginationWrapper>
+    {!isFirst && // se diferente de first redireciona para a page anterior
+      <Link to={prevPage}>Página anterior</Link>
+    }
+    <p>{currentPage} de {numPages}</p>
+    {!isLast && // se diferente de last redireciona para a page próxima
+      <Link to={nextPage}>Próxima página</Link>
+    }
+  </S.PaginationWrapper>
+)
+
+Pagination.propTypes = {
+  isFirst: propTypes.bool.isRequired,
+  isLast: propTypes.bool.isRequired,
+  currentPage: propTypes.number.isRequired,
+  numPages: propTypes.number.isRequired,
+  prevPage: propTypes.string, //link considera string
+  nextPage: propTypes.string,
+}
+
+export default Pagination
